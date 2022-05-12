26baf87c70e4da9cf5edf6c72d731c603e681316
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-12 13:35:08 +0530

    [VINAY] Add: checking the valid Email Address of the User

--- UserRegistrationProblems/Validation.cs
+++ UserRegistrationProblems/Validation.cs
@@ -17,7 +17,15 @@ namespace UserRegistrationProblems
         {
             if (Regex.IsMatch(name, NAME_REGEX)) ;
             {
-                Console.WriteLine("First Name is Valid for the user Account:" + name);
+                Console.WriteLine("Last Name is Valid for the user Account:" + name);
+                return true;
+            }
+        }
+        public bool Validate_EmainAddress(string email)
+        {
+            if (Regex.IsMatch(email, NAME_REGEX)) ;
+            {
+                Console.WriteLine("Email Address is Valid for the user Account:" + email);
                 return true;
             }
         }
--- UserValidationTest/UserValidation.cs
+++ UserValidationTest/UserValidation.cs
@@ -6,19 +6,23 @@ namespace UserValidationTest
 {
     public class Tests
     {
-        
+        Validation input = new Validation();
         [Test]
         public void InputInString_CheckingValidationFor_FirstName()
         {
-            Validation input = new Validation();
              bool result = input.Validate_FirstName("Vinay");
             Assert.IsTrue(result);
         }
         [Test]
         public void InputInString_CheckingValidationFor_LastName()
         {
-            Validation input = new Validation();
             bool result = input.Validate_LastName("Kumer");
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void InputInString_CheckingValidationFor_EmailOfUser()
+        {
+            bool result = input.Validate_EmainAddress("abc.xyz@gmail.co.in");
             Assert.IsTrue(result);
         }
     }
