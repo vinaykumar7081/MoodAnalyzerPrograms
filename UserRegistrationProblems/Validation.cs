7ac08afae0fb975fdadc7c36dab5432bde452a14
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-12 13:43:41 +0530

    [VINAY] Add: checking the valid Email Address of the User Account

--- UserRegistrationProblems/Validation.cs
+++ UserRegistrationProblems/Validation.cs
@@ -5,6 +5,7 @@ namespace UserRegistrationProblems
     public class Validation
     {
         public const string NAME_REGEX = "^[A-Z]{1}[A-Za-z]{3, }$";
+        public const string EMAILADDRESS_REGEX = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([azA-Z]{2,4}|[0-9]{1,3})(\]?)$";
         public bool Validate_FirstName(string name)
         {
             if (Regex.IsMatch(name, NAME_REGEX)) ;
@@ -23,7 +24,7 @@ namespace UserRegistrationProblems
         }
         public bool Validate_EmainAddress(string email)
         {
-            if (Regex.IsMatch(email, NAME_REGEX)) ;
+            if (Regex.IsMatch(email, EMAILADDRESS_REGEX)) ;
             {
                 Console.WriteLine("Email Address is Valid for the user Account:" + email);
                 return true;
